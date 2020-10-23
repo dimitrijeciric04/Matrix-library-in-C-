0a7613e68722f819b0302cbae1432c6ef77757fa
dimitrijeciric04 <62771976+dimitrijeciric04@users.noreply.github.com>
2020-10-23 14:10:01 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@ int random_number(){return rand() % 1000;}
 int main(){
 
 	int n = 100, m = 100;
-	//cin >> n >> m;
+	cin >> n >> m;
 	vector<vector<int>> v1(n, vector<int>(m)), v2(n, vector<int>(m));
 	srand(time(NULL));
 
@@ -16,22 +16,22 @@ int main(){
 			v1[i][j] = random_number();
 			v2[i][j] = random_number();
 		}
-	/*
+	
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
 			cin >> v1[i][j];
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
 			cin >> v2[i][j];
-	*/
+	
 
-	matrix a(v1), b(v2), c(n, m);
+	matrix<int> a(v1), b(v2), c(n, m);
 	auto t1 = chrono::high_resolution_clock::now();
 	c = a;
 	c *= b;
 	auto t2 = chrono::high_resolution_clock::now();
 	cout << chrono::duration_cast<std::chrono::microseconds>( t2 - t1).count() << endl;
-	//c.print();
+	c.print();
 
 	return 0;
 }
