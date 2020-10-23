8292b953045cf99acfb9cfa3fa06cf25a114f84c
dimitrijeciric04 <62771976+dimitrijeciric04@users.noreply.github.com>
2020-10-23 13:39:25 +0200

    Test with random numbers

--- test.cpp
+++ test.cpp
@@ -6,24 +6,32 @@ int random_number(){return rand() % 1000;}
 
 int main(){
 
-	auto t1 = chrono::high_resolution_clock::now();
-
-	int n = 20, m = 20;
+	int n = 100, m = 100;
+	//cin >> n >> m;
 	vector<vector<int>> v1(n, vector<int>(m)), v2(n, vector<int>(m));
 	srand(time(NULL));
+
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++){
 			v1[i][j] = random_number();
 			v2[i][j] = random_number();
 		}
+	/*
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			cin >> v1[i][j];
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			cin >> v2[i][j];
+	*/
 
-	matrix a(v1), b(v2);
-	a *= b;
-	//a.print();
-
+	matrix a(v1), b(v2), c(n, m);
+	auto t1 = chrono::high_resolution_clock::now();
+	c = a;
+	c *= b;
 	auto t2 = chrono::high_resolution_clock::now();
-	cout << chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << endl;
-	//average time is 350 microseconds
+	cout << chrono::duration_cast<std::chrono::microseconds>( t2 - t1).count() << endl;
+	//c.print();
 
 	return 0;
 }
