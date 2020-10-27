0957120d957f3bdc91a75197dfa69a168a103b41
dimitrijeciric04 <62771976+dimitrijeciric04@users.noreply.github.com>
2020-10-27 11:09:33 +0100

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -20,7 +20,7 @@ public:
 		this->n = n;
 		this->m = m;
 	}
-	matrix(const std::vector<std::vector<double>> &x){
+	matrix(const std::vector<std::vector<double>> x){
 		if (x.size() == 0) throw "Matrix size can not be 0";\
 		this->mat.resize(x.size(), std::vector<double>(x[0]));
 		this->n = x.size(), 
@@ -29,7 +29,7 @@ public:
 			for (int j = 0; j < this->m; j++)
 				this->mat[i][j] = x[i][j];
 	}
-	matrix operator=(const matrix &x){
+	matrix operator=(const matrix x){
 		this->n = x.n, this->m = x.m;
 		this->mat.resize(this->n, std::vector<double>(this->m));
 		for (int i = 0; i < this->n; i++)
@@ -37,11 +37,15 @@ public:
 				this->mat[i][j] = x.mat[i][j];
 		return this->mat;
 	}
-	std::vector<double>& operator[](const unsigned int &x){
+	std::vector<double>& operator[](const unsigned int x){
 		if (x > this->n) throw "Index is too big";
 		return this->mat[x];
 	}
-	matrix operator+(matrix &x){
+	void resize(const unsigned int x, const unsigned int y){
+		this->n = x, this->m = y;
+		mat.resize(x, std::vector<double>(y));
+	}
+	matrix operator+(matrix x){
 		if (this->n != x.n || this->m != x.m)
 			throw "You can not add matrices with different size\n";
 		matrix new_mat(this->n, this->m);
@@ -50,7 +54,7 @@ public:
 				new_mat[i][j] = this->mat[i][j] + x[i][j];
 		return new_mat;
 	}
-	matrix operator+=(matrix &x){
+	matrix operator+=(matrix x){
 		if (this->n != x.n || this->m != x.m)
 			throw "You can not add matrices with different size\n";
 		for (int i = 0; i < this->n; i++)
@@ -58,21 +62,21 @@ public:
 				this->mat[i][j] += x[i][j];
 		return this->mat;
 	}
-	matrix operator*(const double &x){
+	matrix operator*(const double x){
 		matrix new_mat(this->n, this->m);
 		for (int i = 0; i < this->n; i++)
 			for (int j = 0; j < this->m; j++)
 				new_mat[i][j] = this->mat[i][j] * x;
 		return new_mat;
 	}
-	matrix operator*=(const double &x){
+	matrix operator*=(const double x){
 		for (int i = 0; i < this->n; i++)
 			for (int j = 0; j < this->m; j++)
 				this->mat[i][j] *= x;
 		return this->mat;
 	}
 private:
-	static void process(matrix &new_mat, matrix &a, matrix &b, int i, int n, int m){
+	static void process_multiplication(matrix &new_mat, matrix &a, matrix &b, int i, int n, int m){
 		double tmp;
 		for (int j = 0; j < n; j++){
 			tmp = 0;
@@ -88,13 +92,13 @@ public:
 		matrix new_mat(this->n, x.m), tmp_mat(this->mat);
 		std::vector<std::thread> threads(this->n);
 		for (int i = 0; i < this->n; i++)
-			threads[i] = std::thread(std::ref(process), std::ref(new_mat), 
+			threads[i] = std::thread(std::ref(process_multiplication), std::ref(new_mat), 
 				std::ref(tmp_mat), std::ref(x), i, x.m, this->m);
 		/*
-		T tmp; // calculation without multithreading
+		double tmp; // calculation without multithreading
 		for (int i = 0; i < new_mat.n; i++)
 			for (int j = 0; j < new_mat.m; j++){
-				tmp = this->neutral_element;
+				tmp = 0;
 				for (int k = 0; k < this->m; k++)
 					tmp += this->mat[i][k] * x[k][j];
 				new_mat[i][j] = tmp;
@@ -110,13 +114,13 @@ public:
 		matrix new_mat(this->n, x.m), tmp_mat(this->mat);
 		std::vector<std::thread> threads(this->n);
 		for (int i = 0; i < this->n; i++)
-			threads[i] = std::thread(std::ref(process), std::ref(new_mat), 
+			threads[i] = std::thread(std::ref(process_multiplication), std::ref(new_mat), 
 				std::ref(tmp_mat), std::ref(x), i, x.m, this->m);
 		/*
-		T tmp; // calculation without multithreading
+		double tmp; // calculation without multithreading
 		for (int i = 0; i < new_mat.n; i++)
 			for (int j = 0; j < new_mat.m; j++){
-				tmp = this->neutral_element;
+				tmp = 0;
 				for (int k = 0; k < this->m; k++)
 					tmp += this->mat[i][k] * x[k][j];
 				new_mat[i][j] = tmp;
@@ -162,6 +166,7 @@ public:
 				this->mat[i][j] = tmp_mat[i][j];
 		return matrix(this->mat);
 	}
+private:
 	double determinant(matrix x){
 		if (x.n != x.m)
 			throw "Determinant is not supported for non-square matrix!";
@@ -169,10 +174,40 @@ public:
 		if (x.n == 2) return x[0][0] * x[1][1] - x[0][1] * x[1][0];
 		double result = 0;
 		for (int i = 0; i < x.n; i++)
-			result += x[0][i] * determinant(x ^ std::make_pair(0, i)) * pow(-1, i);
+			result += x[0][i] * determinant(x ^ std::make_pair(0, i)) * (i % 2? -1 : 1);
 		return result;
 	} 
+public:
 	double determinant(){return determinant(this->mat);}
+	matrix transpose(){
+		matrix new_mat(this->m, this->n), tmp_mat(this->mat);
+		for (int i = 0; i < this->n; i++)
+			for (int j = 0; j < this->m; j++)
+				new_mat[j][i] = this->mat[i][j];
+		return new_mat;
+	}
+private:
+	static void process_inverse(int i, int m, double det, matrix &inverse, matrix &this_mat){
+		for (int j = 0; j < m; j++)
+			inverse[i][j] = (this_mat ^ std::make_pair(i, j)).determinant() / det * ((i + j) % 2? -1 : 1);
+	}
+public:
+	matrix operator~(){ // inverse of matrix
+		double det = this->determinant();
+		matrix inverse(this->n, this->m), this_mat(this->mat);
+		std::vector<std::thread> threads(this->n);
+		for (int i = 0; i < this->n; i++)
+			threads[i] = std::thread(std::ref(process_inverse), i, this->m, det, std::ref(inverse), std::ref(this_mat));
+		for (int i = 0; i < this->n; i++)
+			threads[i].join();
+		/*
+		for (int i = 0; i < this->n; i++)
+			for (int j = 0; j < this->m; j++)
+				inverse[i][j] = (this_mat ^ std::make_pair(i, j)).determinant() / det * ((i + j) % 2? -1 : 1);
+		*/
+		return inverse.transpose();
+	}
+	
 	void print(){
 		for (int i = 0; i < this->n; i++){
 			for (int j = 0; j < this->m; j++)
